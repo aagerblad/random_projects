cd108e3bed25893b53e55a5e2d0f8e0b9cfdba35
Andreas Agerblad <aagerblad@spotify.com>
2022-01-24 17:17:03 -0500

    Better animation

--- src/lines_2/script.js
+++ src/lines_2/script.js
@@ -5,6 +5,23 @@ function setup() {
 }
 
 function draw() {
+  next_step();
+  background("#222");
+
+  for(let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].steps.length; j++) {
+      with(lines[i]){
+        with(steps[j]) {
+          stroke(a, b, c , opac);
+          strokeWeight(stroke_weight);  
+          line(x1, y1, x2, y2);
+        }
+      }
+    }
+  }
+}
+
+function next_step() {
   if(random(0, 10) < 1) {
     lines.push({
       x1: 0,
@@ -16,26 +33,43 @@ function draw() {
       r4: random(20, 23),
       r5: random(3, 9),
       r6: random(1, 1),
+      stroke_weight: random(1, 5),
+      opac: 255,
+      steps: [],
     })
   }
 
 
   for(let i = 0; i < lines.length; i++) {
+    if (lines[i].opac <= 0) {
+      lines.splice(i, 1);
+      continue;
+    }
     with(lines[i]) {
       x2 = x1 + r4;
       y2 = y1 + r1;
       r1 -= 1;
+      opac -= 2;
+
 
       a = 256 - Math.abs(x2 - y2) / 7;
       b = 40 + Math.abs(x2 - y2) / 3;
       c = c + r5;
       r3 = random(-15, 15);
-      stroke(a + r3 + c / 5, b + r3 + c / 3, c + r3);
-      strokeWeight(6);
-      line(x1, y1, x2, y2);
+
+      steps.push( {
+        a: a + r3 + c / 5,
+        b: b + r3 + c / 3,
+        c: c + r3 + c,
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2,
+      })
 
       x1 = x2;
       y1 = y2;
     }
   }
+
 }
