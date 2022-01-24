13bd6c0b0732e7b113179b9808a531bdf75c8a29
Andreas Agerblad <aagerblad@spotify.com>
2022-01-24 16:28:21 -0500

    Cool lines

--- src/lines_2/script.js
+++ src/lines_2/script.js
@@ -1,24 +1,25 @@
-const MAX_HEIGHT = (window.innerHeight * 2) / 3;
-const MAX_WIDTH = (window.innerWidth * 2) / 3;
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  noLoop();
+  // noLoop();
+  loops = 0;
 }
 
 function draw() {
-  for (x = 0; x < 100; x++) {
+  if (loops < 100) {
+    loops += 1;
     c = 30;
 
-    x1 = random(10, 30);
-    y1 = random(10,120);
-    x2 = x1 + 10;
-    y2 = y1 + 10;
-
-    r1 = random(10, 40);
+    r0 = random(0, 800);
+    r1 = random(0, 30);
     r2 = random(-5, 15);
-    r4 = random(15, 17);
-    r5 = random (3,7)
-    r6 = random(1,1)
+    r4 = random(20, 23);
+    r5 = random(3, 9);
+    r6 = random(1, 1);
+
+    x1 = 0;
+    y1 = r0;
+    x2 = x1;
+    y2 = y1;
     for (let i = 0; i < 150; i++) {
       x1 = x2;
       y1 = y2;
@@ -26,11 +27,11 @@ function draw() {
       y2 = y2 + r1;
       r1 -= 1;
 
-      a = 20 + Math.abs(x2 - y2) / r6;
-      b = 20 + Math.abs(x2 - y2) / 3;
+      a = 256 - Math.abs(x2 - y2) / 7;
+      b = 40 + Math.abs(x2 - y2) / 3;
       c = c + r5;
       r3 = random(-15, 15);
-      stroke(a + r3, b + r3, c + r3);
+      stroke(a + r3 + c / 5, b + r3 + c / 3, c + r3);
       line(x1, y1, x2, y2);
     }
   }
