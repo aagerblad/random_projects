222bc1d052460af161a97780442823af172afc14
Andreas Agerblad <aagerblad@spotify.com>
2022-01-24 16:47:24 -0500

    animated lines

--- src/lines_2/script.js
+++ src/lines_2/script.js
@@ -1,30 +1,29 @@
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   // noLoop();
-  loops = 0;
+  lines = []
 }
 
 function draw() {
-  if (loops < 100) {
-    loops += 1;
-    c = 30;
+  if(random(0, 10) < 1) {
+    lines.push({
+      x1: 0,
+      y1: random(0, window.innerHeight),
+      c: 30,
+      r0: random(0, window.innerHeight),
+      r1: random(0, 30),
+      r2: random(-5, 15),
+      r4: random(20, 23),
+      r5: random(3, 9),
+      r6: random(1, 1),
+    })
+  }
 
-    r0 = random(0, 800);
-    r1 = random(0, 30);
-    r2 = random(-5, 15);
-    r4 = random(20, 23);
-    r5 = random(3, 9);
-    r6 = random(1, 1);
 
-    x1 = 0;
-    y1 = r0;
-    x2 = x1;
-    y2 = y1;
-    for (let i = 0; i < 150; i++) {
-      x1 = x2;
-      y1 = y2;
-      x2 = x2 + r4;
-      y2 = y2 + r1;
+  for(let i = 0; i < lines.length; i++) {
+    with(lines[i]) {
+      x2 = x1 + r4;
+      y2 = y1 + r1;
       r1 -= 1;
 
       a = 256 - Math.abs(x2 - y2) / 7;
@@ -33,6 +32,9 @@ function draw() {
       r3 = random(-15, 15);
       stroke(a + r3 + c / 5, b + r3 + c / 3, c + r3);
       line(x1, y1, x2, y2);
+
+      x1 = x2;
+      y1 = y2;
     }
   }
 }
