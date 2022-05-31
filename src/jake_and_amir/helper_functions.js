c00c39a16718340e832d1383dc8d189ee03971a8
Andreas Agerblad <aagerblad@spotify.com>
2022-05-31 19:39:11 -0400

    Barely fixed anything

---
+++ src/jake_and_amir/helper_functions.js
@@ -0,0 +1,51 @@
+function score_mapper(score) {
+  if (score == "" || score == null || score.includes("N/A")) {
+    return null;
+  }
+  s = 0;
+  if (score.includes("F")) {
+    s = 0;
+  } else if (score.includes("D")) {
+    s = 1;
+  } else if (score.includes("C")) {
+    s = 2;
+  } else if (score.includes("B")) {
+    s = 3;
+  } else if (score.includes("A")) {
+    s = 4;
+  }
+  if (score.includes("+")) {
+    s += 0.333;
+  } else if (score.includes("-")) {
+    s -= 0.333;
+  }
+  return s;
+}
+
+var data_mapper = function (d) {
+  jake_score = score_mapper(d["Jake Grade"]);
+    amir_score = score_mapper(d["Amir Grade"]);
+    guest_score = score_mapper(d["Guest \nGrade"]);
+
+    both_score = null;
+
+    if (jake_score == amir_score) {
+      both_score = jake_score;
+      jake_score = null;
+      amir_score = null;
+    }
+
+    return {
+      date: Date.parse(d["Date"]),
+      no: d["No."],
+      jake_score: jake_score,
+      jake_grade: d["Jake Grade"],
+      amir_score: amir_score,
+      amir_grade: d["Amir Grade"],
+      guest_name: d["Guest"],
+      guest_score: guest_score,
+      guest_grade: d["Guest \nGrade"],
+      both_score: both_score,
+      title: d["Title"].replace(/\"/g, ""),
+    };
+}
--- src/jake_and_amir/script.js
+++ src/jake_and_amir/script.js
@@ -1,20 +1,120 @@
+// FUNCTIONS
+
+var mouseover = function (d) {
+  data = d.srcElement.__data__;
+  date = new Date(data.date);
+  console.log("mouseover");
+  d3.select("#tooltip")
+    .transition()
+    .duration(100)
+    .style("opacity", 1)
+    .text(
+      data.title +
+        "\n Episode no. " +
+        data.no +
+        "\n" +
+        date.toDateString() +
+        "\n" +
+        "Jake: " +
+        data.jake_grade +
+        "\n" +
+        "Amir: " +
+        data.amir_grade +
+        "\n" +
+        data.guest_name +
+        ": " +
+        data.guest_grade
+    );
+
+  d3.selectAll("#episode_" + data.no)
+    .style("stroke", "white")
+    .style("opacity", 1);
+
+  // d3.select(this).style("stroke", "white").style("opacity", 1);
+};
+
+var mousemove = function (d) {
+  d3.select("#tooltip")
+    .style("left", d.x + 10 + "px")
+    .style("top", d.y + 10 + "px");
+};
+
+var mouseleave = function (d) {
+  d3.select("#tooltip").style("opacity", 0).text("");
+  d3.selectAll("#episode_" + data.no)
+    .style("stroke", "black")
+    .style("opacity", 0.8);
+};
+
+function add_dots(data, color, yScaleFun) {
+  g.selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return xScale(d.date);
+    })
+    .attr("cy", yScaleFun)
+    .attr("id", function (d) {
+      return "episode_" + d.no;
+    })
+    .attr("r", 5)
+    .attr("fill", color)
+    .style("opacity", 0.8)
+    .style("stroke", "black")
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
+}
+
+// Start of SVG
+
 const svg = d3.select("#chart").append("svg").attr("viewBox", `0 0 800 800`);
 
-// Amir #e85c94
-// Jake #688cc4
+var defs = svg.append("defs");
 
-var margin = 0,
+var gradient = defs
+  .append("linearGradient")
+  .attr("id", "svgGradient")
+  .attr("x1", "0%")
+  .attr("x2", "100%")
+  .attr("y1", "0%")
+  .attr("y2", "100%");
+
+gradient
+  .append("stop")
+  .attr("class", "start")
+  .attr("offset", "45%")
+  .attr("stop-color", "#e85c94")
+  .attr("stop-opacity", 1);
+
+gradient
+  .append("stop")
+  .attr("class", "end")
+  .attr("offset", "50%")
+  .attr("stop-color", "#ffffff")
+  .attr("stop-opacity", 1);
+
+gradient
+  .append("stop")
+  .attr("class", "end")
+  .attr("offset", "55%")
+  .attr("stop-color", "#688cc4")
+  .attr("stop-opacity", 1);
+
+var margin = 100,
   svg_width = 600,
   svg_height = 400;
 
 var mindate = new Date(2007, 1, 1),
   maxdate = new Date(2016, 1, 1);
 
-// var xScale = d3.scaleBand().range([0, svg_width]).padding(0.5),
 var xScale = d3.scaleTime().domain([mindate, maxdate]).range([0, svg_width]),
-  yScale = d3.scaleLinear().range([svg_height, 0]);
+  yScale = d3.scaleLinear().domain([0, 4.5]).range([svg_height, 0]);
 
-var g = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
+var g = svg
+  .append("g")
+  .attr("transform", "translate(" + margin + "," + margin + ")");
 
 d3.csv(
   "https://docs.google.com/spreadsheets/d/e/" +
@@ -22,62 +122,7 @@ d3.csv(
     "ll9RbsQ6vpxUj6_fC1MPvdus8tfyNsBi2fMqt5ocf" +
     "Ijvf/pub?gid=0&single=true&output=csv"
 ).then(function (raw_data) {
-  function score_mapper(score) {
-    if (score.includes("N/A")) {
-      console.log(score)
-      return null;
-    } 
-    if (score == "" || score == null || score.includes("N/A")) {
-      return null;
-    }
-    s = 0;
-    if (score.includes("F")) {
-      s = 0;
-    } else if (score.includes("D")) {
-      s = 1;
-    } else if (score.includes("C")) {
-      s = 2;
-    } else if (score.includes("B")) {
-      s = 3;
-    } else if (score.includes("A")) {
-      s = 4;
-    }
-    if (score.includes("+")) {
-      s += 0.333;
-    } else if (score.includes("-")) {
-      s -= 0.333;
-    }
-    return s;
-  }
-
-  data = raw_data.map(function (d) {
-    jake_score = score_mapper(d["Jake Grade"]);
-    amir_score = score_mapper(d["Amir Grade"]);
-
-    if(d["Amir Grade"] == "N/A") {
-      console.log(d)
-      console.log(amir_score)
-    }
-
-    both_score = null;
-
-    if (jake_score == amir_score) {
-      both_score = jake_score;
-      jake_score = null;
-      amir_score = null;
-    }
-
-    return {
-      date: Date.parse(d["Date"]),
-      no: d["No."],
-      jake_score: jake_score,
-      jake_grade: d["Jake Grade"],
-      amir_score: amir_score,
-      amir_grade: d["Amir Grade"],
-      both_score: both_score,
-      title: d["Title"].replace(/\"/g, ""),
-    };
-  });
+  data = raw_data.map(data_mapper);
 
   data = data.filter(function (d) {
     return d.jake_score != null || d.amir_score != null || d.both_score != null;
@@ -94,60 +139,9 @@ d3.csv(
     .style("border-radius", "5px")
     .style("padding", "5px");
 
-  console.log(data);
-
-  // Three function that change the tooltip when user hover / move / leave a cell
-  var mouseover = function (d) {
-    data = d.srcElement.__data__;
-    date = new Date(data.date);
-    console.log("mouseover");
-    d3.select("#tooltip")
-      .transition()
-      .duration(100)
-      .style("opacity", 1)
-      .text(
-        data.title +
-          "\n Episode no. " +
-          data.no +
-          "\n" +
-          date.toDateString() +
-          "\n" +
-          "Jake: " +
-          data.jake_grade +
-          "\n" +
-          "Amir: " +
-          data.amir_grade
-      );
-
-    d3.selectAll("#episode_" + data.no)
-      .style("stroke", "white")
-      .style("opacity", 1);
-
-    // d3.select(this).style("stroke", "white").style("opacity", 1);
-  };
-
-  var mousemove = function (d) {
-    d3.select("#tooltip")
-      .style("left", d.x + 10 + "px")
-      .style("top", d.y + 10 + "px");
-  };
-
-  var mouseleave = function (d) {
-    d3.select("#tooltip").style("opacity", 0).text("");
-    d3.selectAll("#episode_" + data.no)
-      .style("stroke", "black")
-      .style("opacity", 0.5);
-  };
-
-  yScale.domain([
-    0,
-    d3.max(data, function (d) {
-      return d.amir_score;
-    }),
-  ]);
-
+  // X-axis
   g.append("g")
-    .attr("transform", "translate(0," + svg_height + ")")
+    .attr("transform", "translate(0," + svg_height + ")") // move to bottom
     .attr("color", "white")
     .call(
       d3
@@ -156,101 +150,59 @@ d3.csv(
         .tickValues(data.date)
         .ticks(10)
     );
-  // .append("text")
-  // .attr("y", svg_height - 450)
-  // .attr("x", svg_width - 100)
-  // .attr("text-anchor", "end")
-  // .attr("stroke", "white")
-  // .text("Year");
 
-  g.append("g").call(d3.axisLeft(yScale)).attr("color", "white");
-  // .append("text")
-  // .attr("transform", "rotate(-90)")
-  // .attr("y", 0)
-  // .attr("dy", "-5.1em")
-  // .attr("text-anchor", "end")
-  // .attr("stroke", "black")
-  // .text("Score");
+  // Y-axis
+  g.append("g")
+    .call(d3.axisLeft(yScale))
+    .attr("id", "y-axis")
+    .attr("color", "white");
 
-  var defs = svg.append("defs");
+  function enter_chart() {
+    add_dots(
+      data.filter(function (d) { return d.amir_score != null; }),
+      "#e85c94",
+      function (d) { return yScale(d.amir_score); }
+    );
 
-  var gradient = defs
-    .append("linearGradient")
-    .attr("id", "svgGradient")
-    .attr("x1", "0%")
-    .attr("x2", "100%")
-    .attr("y1", "0%")
-    .attr("y2", "100%");
+    add_dots(
+      data.filter(function (d) { return d.jake_score != null; }),
+      "#688cc4",
+      function (d) { return yScale(d.jake_score); }
+    );
 
-  gradient
-    .append("stop")
-    .attr("class", "start")
-    .attr("offset", "45%")
-    .attr("stop-color", "#e85c94")
-    .attr("stop-opacity", 1);
+    add_dots(
+      data.filter(function (d) { return d.guest_score != null; }),
+      "#ffb42c",
+      function (d) { return yScale(d.guest_score); }
+    );
 
-  gradient
-    .append("stop")
-    .attr("class", "end")
-    .attr("offset", "50%")
-    .attr("stop-color", "#ffffff")
-    .attr("stop-opacity", 1);
-
-  gradient
-    .append("stop")
-    .attr("class", "end")
-    .attr("offset", "55%")
-    .attr("stop-color", "#688cc4")
-    .attr("stop-opacity", 1);
-
-  function add_dots(data, color, yScaleFun) {
-    g.selectAll("dot")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d) {
-        return xScale(d.date);
-      })
-      .attr("cy", yScaleFun)
-      .attr("id", function (d) {
-        return "episode_" + d.no;
-      })
-      .attr("r", 5)
-      .attr("fill", color)
-      .style("opacity", 0.5)
-      .style("stroke", "black")
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave);
+    add_dots(
+      data.filter(function (d) { return d.both_score != null; }),
+      "url(#svgGradient)",
+      function (d) { return yScale(d.both_score); }
+    );
   }
 
-  add_dots(
-    data.filter(function (d) {
-      return d.jake_score != d.amir_score && d.amir_score != null;
-    }),
-    "#e85c94",
-    function (d) {
-      return yScale(d.amir_score);
-    }
-  );
+  var slider = d3
+    .sliderHorizontal()
+    .min(0)
+    .max(10)
+    .step(1)
+    .width(300)
+    .displayValue(false)
+    .on("onchange", (val) => {
+      console.log(val);
+      d3.select("#y-axis").call(d3.axisLeft(yScale.domain([0, val])));
+      enter_chart();
+    });
 
-  add_dots(
-    data.filter(function (d) {
-      return d.jake_score != d.amir_score && d.jake_score != null;
-    }),
-    "#688cc4",
-    function (d) {
-      return yScale(d.jake_score);
-    }
-  );
+  d3.select("#slider")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 100)
+    .append("g")
+    .attr("transform", "translate(30,30)")
+    .call(slider);
 
-  add_dots(
-    data.filter(function (d) {
-      return d.both_score;
-    }),
-    "url(#svgGradient)",
-    function (d) {
-      return yScale(d.both_score);
-    }
-  );
+  enter_chart();
 });
